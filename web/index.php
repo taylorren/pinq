ec13b4c4ee3eb818a55e1e7f5a45dbd51055670b
TR@SOE <taylor.ren@gmail.com>
2014-06-19 21:55:15 +0800

    Move on.

--- web/index.php
+++ web/index.php
@@ -36,14 +36,33 @@ $app->get('/demo1', function () use ($app)
     global $demo;
     $books = $demo->test1($app);
     $data = Traversable::from($books);
-    
-    //Apply first filter
-    $filter1=$data
-            ->where(function($row){return $row['price']>95;})
-            ->orderByDescending(function($row){return $row['id'];});
 
+    //Apply first filter
+    $filter1 = $data
+            ->where(function($row)
+            {
+                return $row['price'] > 90 && $row['price'] < 99;
+            })
+            ->orderByDescending(function($row)
+    {
+        return $row['id'];
+    });
+
+    $filter2 = $filter1
+            ->groupBy(function($row)
+            {
+                return $row['author'];
+            })
+            ->select(
+            function(ITraversable $filter1)
+    {
+        return ['author' => $filter1->first()['author'], 'count' => $filter1->count()];
+    }
+    );
+
+    //var_dump($filter2);
     //return $app['twig']->render('demo1.html.twig', array('books'=>$books));
-    return $app['twig']->render('demo1.html.twig', array('orig' => $data, 'filter1'=>$filter1));
+    return $app['twig']->render('demo1.html.twig', array('orig' => $data, 'filter1' => $filter1, 'filter2'=>$filter2));
 }
 );
 
